b1bab9681fd9f9f2d9e119e260d198b1aec76ae2
Sergei <volsergmih@gmail.com>
2024-10-11 09:14:36 +0300

    docker practice hw done

--- 19. Virtualization/19.4 Docker Practice/vm/ansible.tf
+++
@@ -1,7 +0,0 @@
-resource "local_file" "hosts" {
-  content = templatefile("${path.module}/hosts.tftpl",
-  { 
-    app =  [ yandex_compute_instance.app ]
-  } )
-  filename = "${path.module}/hosts"
-}
